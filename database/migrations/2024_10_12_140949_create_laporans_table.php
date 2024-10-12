6434ce8dd156b1944df75a708293df3b08c50922
azkiya-mufida-melon <azkiyamufida23@mhs.politala.ac.id>
2024-10-12 22:33:54 +0800

    mengubah

--- database/migrations/2024_10_12_140949_create_laporans_table.php
+++ database/migrations/2024_10_12_140949_create_laporans_table.php
@@ -10,12 +10,15 @@ return new class extends Migration
      * Run the migrations.
      */
     public function up(): void
-    {
-        Schema::create('laporans', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
+{
+    Schema::create('laporans', function (Blueprint $table) {
+        $table->id('id_laporan'); // ID untuk laporan
+        $table->bigInteger('id_pesanan'); // Relasi ke tabel pesanan (foreign key)
+        $table->date('tgl_laporan'); // Tanggal laporan
+        $table->timestamps(); // timestamps untuk created_at dan updated_at
+    });
+}
+
 
     /**
      * Reverse the migrations.
